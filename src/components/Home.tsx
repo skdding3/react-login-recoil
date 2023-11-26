d12935c79a6df77285ee62b2f44e7ac74542d7fd
skdding3 <skdding3@gmail.com>
2023-11-26 16:30:03 +0900

    typescript version change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Button } from "@material-tailwind/react";
+import { Button, Input } from "@material-tailwind/react";
 
 const Home = () => {
   return (
     <>
-      <div className="flex items-center">
-        <h1>Login</h1>
-        <input type="text"></input>
+      <div className="flex flex-col justify-center items-center gap-5 py-72">
+        <h1>로그인 하기</h1>
+        <div className="flex flex-col w-72 gap-2">
+          <Input type="text" label="username" />
+          <Input type="password" label="password" />
+        </div>
         <Button>Button</Button>
       </div>
     </>
